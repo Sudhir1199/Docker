b74ef595e465eb025c2342619048b7a3025a13c3
Sudhir <60312216+Sudhir1199@users.noreply.github.com>
2024-04-11 22:04:59 +0530

    Spring Boot

---
+++ src/main/java/com/example/sudhir/Docker/Controller/DockerDemoController.java
@@ -0,0 +1,16 @@
+package com.example.sudhir.Docker.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerDemoController {
+
+    @GetMapping("/testAPI")
+    public String testMethod(){
+        return "I am working fine";
+    }
+
+}
